834aaffb6fc3cf0e0341151314131d04a8556bf9
Venkata Dasu <vdasu@mail.usf.edu>
2019-10-07 14:48:03 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace print_series_n_terms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = 10;
+                int count = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    if (count == n)
+                    {
+                        break;
+                    }
+
+                    else
+                    {
+                        for (int j = 1; j <= i; j++)
+                        {
+                            if (count == n)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.Write(i + ",");
+                                count++;
+                            }
+
+                        }
+                    }
+                }
+
+            }   // End of try
+
+            catch
+            {
+                Console.WriteLine("Exception occured while computing Intersect()");
+
+            }   // End of catch
+        }
+    }
+}
